516a4d86ffaf236fc434de1b79cc78b17191535c
Jennifer <jennespinozavidal@gmail.com>
2023-06-18 18:45:39 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+import Listado from './Components/Listado'
+import Formulario from './Components/Formulario'
+import Buscador from './Components/Buscador'
+import Alert from './Components/Alert'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
